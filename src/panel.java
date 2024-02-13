00817cd8b8f176db67e541721e363f58b115c197
Imran-2022 <mdimranulhaque202@gmail.com>
2024-02-13 10:41:29 +0600

    completed

---
+++ src/panel.java
@@ -0,0 +1,19 @@
+
+import java.awt.Graphics;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author imran
+ */
+class panel {
+
+    static void paint(Graphics graphics) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
